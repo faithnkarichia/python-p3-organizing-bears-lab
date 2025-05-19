2e2192e7dae4a54ecb645c97c5d36db7236c2ce1
Faith <kashfaith77@gmail.com>
2025-05-19 18:12:48 +0300

    SQL querries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",5,"M","Black","Playful",True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",10,"F","grey","social",True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",2,"M","white","Playful",True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",1,"M","red","empathetic",False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",7,"F","brown","Playful",False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",5,"F","cinnamon","joyful",True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",3,"M","blond","social",False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Null,8,"F","Black","Playful",True);
